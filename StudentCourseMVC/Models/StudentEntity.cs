b9b95221dcc67d9373f5f6b4f57353550da7f51b
RIFAT AMIN <khrifatamin@gmail.com>
2025-06-21 23:42:21 +0600

    From rifat

--- StudentCourseMVC/Models/StudentEntity.cs
+++ StudentCourseMVC/Models/StudentEntity.cs
@@ -1,10 +1,15 @@
-﻿namespace StudentCourseMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCourseMVC.Models
 {
     public class StudentEntity
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Required]
         public string CourseTitle { get; set; } = string.Empty;
+        [Required]
 
         public double TotalMarks { get; set; }
         public StatusEnum Status
